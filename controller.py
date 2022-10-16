9e70a1c574c76490bb2abaeee6413f4ab557763e
allajka <allajka94@gmail.com>
2022-10-16 16:39:03 +0300

    version 3.0

--- controller.py
+++ controller.py
@@ -6,10 +6,10 @@ def button_click():
     ui.print_data("Вы можете: \n1. Вывести всю информацию\n2. Добавить информацию")
     user_answer = ui.input_data()
     if user_answer == "1":
-        list_data = in_out.get_info("general_work_personnel\PI.csv")  
-        list_data += in_out.get_info("general_work_personnel\Salary.csv")  
+        list_data = in_out.get_info("general_work_personnel\PI.csv.txt")  
+        # list_data += in_out.get_info("general_work_personnel\Salary.csv")  
         #list_data += in_out.get_info("name_file")  # дописать 
-        print(list_data)
+        ui.print_all(list_data)
         logger.info_logger(f'Запрос на вывод информации')
     elif user_answer == 2:
         data_personal = UI.add_data()
--- ui.py
+++ ui.py
@@ -11,3 +11,8 @@ def add_data():
     patronymic = input("patronymic: ")
     phone = input("phone: ")
     return f"phone: +7 {phone} surname: {surname} name: {name} patronymic: {patronymic}"
+
+
+def print_all(data):
+    for i in data:
+        print(i)
