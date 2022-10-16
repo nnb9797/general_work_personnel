8d0e1c9c1f27eb64628166e107f305db374cefff
allajka <allajka94@gmail.com>
2022-10-16 11:25:53 +0300

    in_out

---
+++ in_out.py
@@ -0,0 +1,9 @@
+def append_data(name_file, data):
+    with open(name_file, 'a') as file:
+        file.write(f"{data},")
+
+
+def get_info(name_file):
+    with open(name_file, 'r') as file:
+        info = file.read().split(",")
+    return info
