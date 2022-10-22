eb70a2250001e98e8ed3d9d9a99c18d87c153a77
Alexey Maslov <alvladigital@gmail.com>
2022-10-23 00:47:58 +0300

    added active list + mark in logger

--- controller.py
+++ controller.py
@@ -1,14 +1,21 @@
 import ui
 from answers import *
+from logger import *
 
 
 def button_click():
-    ui.print_data("Вы можете: \n1. Вывести всю информацию\n2. Добавить информацию \n3. Поиск по ID")
-    user_answer = ui.input_data("Введите цифру: ")
-    print()
-    if user_answer == "1":
-        print_all()
-    elif user_answer == "2":
-        add_data()
-    elif user_answer == "3":
-        search_data()
+    info_logger(f'Справочник запущен')
+    while True:
+        ui.print_data("Вы можете: \n1. Вывести всю информацию\n2. Добавить информацию \n3. Поиск по ID \n4. Закрыть программу")
+        user_answer = ui.input_data("Введите цифру: ")
+        print()
+        if user_answer == "1":
+            print_all()
+        elif user_answer == "2":
+            add_data()
+        elif user_answer == "3":
+            search_data()
+        elif user_answer == "4":
+            print("Справочник закрыт")
+            info_logger(f'Справочник закрыт')
+            break
