6a3c8b108794f1c9d0c379824d52b633ce7773d6
Alexey Maslov <alvladigital@gmail.com>
2022-10-23 00:49:38 +0300

    add check id use add_data

--- answers.py
+++ answers.py
@@ -2,7 +2,6 @@ from in_out import *
 from logger import *
 from ui import *
 
-
 def print_all():
     list_data = get_info("PI.csv.txt")
     list_data += get_info("Salary.csv.txt")
@@ -14,20 +13,28 @@ def print_all():
 
 def add_data():
     id_pes = input_data("Введите id: ")
+    list_data = get_info("PI.csv.txt")
+    count = 0
+    for i in list_data:
+        if f"ID {id_pes}" in i:
+            print("Введеный id уже существует")
+            info_logger(f'Введеный id уже существует')
+            count += 1
+            break
+    if count == 0:
+        data_personal = add_data_pi(id_pes)
+        append_data("PI.csv.txt", data_personal)
+        info_logger(f'Новая запись в таблицу "PI" {data_personal}')
 
-    data_personal = add_data_pi(id_pes)
-    append_data("PI.csv.txt", data_personal)
-    info_logger(f'Новая запись в таблицу "PI" {data_personal}')
+        data_personal = add_data_Salary(id_pes)
+        append_data("Salary.csv.txt", data_personal)
+        info_logger(f'Новая запись в таблицу "Salary" {data_personal}')
 
-    data_personal = add_data_Salary(id_pes)
-    append_data("Salary.csv.txt", data_personal)
-    info_logger(f'Новая запись в таблицу "Salary" {data_personal}')
+        data_personal = add_data_Department(id_pes)
+        append_data("Department.csv.txt", data_personal)
+        info_logger(f'Новая запись в таблицу "Department" {data_personal}')
 
-    data_personal = add_data_Department(id_pes)
-    append_data("Department.csv.txt", data_personal)
-    info_logger(f'Новая запись в таблицу "Department" {data_personal}')
-
-    print_data("\nДанные успешно добавлены.")
+        print_data("\nДанные успешно добавлены.")
 
 def search_data():
     id_pes = input("Введите id: ")
